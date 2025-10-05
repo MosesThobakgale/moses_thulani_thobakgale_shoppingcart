76a067e46c38b778e1237457c51c702d27416d9c
Moses Thulani Thobakgale <thulani.thobakgale@gmail.com>
2025-10-05 20:42:33 +0200

    feat(models): move CartItem model to own file, the code has helpers and change price type to BigDecimal

--- CartItem.java
+++ CartItem.java
@@ -1,3 +1,39 @@
+import java.math.BigDecimal;
+
 public class CartItem {
-    
+    private String name;
+    private BigDecimal price; // BigDecimal for currency, better precision
+    private int quantity;
+
+    public CartItem(String name, BigDecimal price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+
+    // Setters
+    public void increaseQuantity(int qty) {
+        this.quantity += qty;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
